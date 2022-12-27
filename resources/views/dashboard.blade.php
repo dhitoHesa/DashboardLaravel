ca0c7b75133530662ac2bc2b22772d703596e38b
dhitoHesa <radhityohernanda@gmail.com>
2022-12-27 17:54:49 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<head>
+    <!-- Styles -->
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body class="bg-danger-subtle">
+    <div class="container">
+        <ul>
+        @foreach($data as $data)
+            <li>
+                <p class="m-1"><b>Nama:</b> {{$data['nama']}}</p>
+                <p><b>Sekolah:</b> {{$data['school']}}</p>
+            </li>
+        @endforeach
+        </ul>
+    </div>
+</body>
