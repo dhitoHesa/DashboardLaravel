5db0d295e90400ba5683a78c4884b738b79b4f0d
dhitoHesa <radhityohernanda@gmail.com>
2022-12-28 15:28:06 +0700

    route update

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('profiles', [App\Http\Controllers\Profiles::class, 'list']);
 
 Route::get('/', [App\Http\Controllers\Dashboard::class, 'list']);
